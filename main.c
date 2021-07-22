4c7e894ca78683837b0dc605291f2624f53e5cfe
Hilmabcd <78993982+Hilmabcd@users.noreply.github.com>
2021-07-21 20:23:08 -0600

    first main

---
+++ main.c
@@ -0,0 +1,52 @@
+/* Program : main.c
+ * Deskripsi : main Aplikasi Layanan Dokter Hewan
+ * Nama : Hilma Sri Rahayu
+ * Tanggal : 22 Juli 2021
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include "queue.h"
+
+int main (){
+	Queue Q;
+	CreateQueue (&Q);
+	
+	int menuPilihan;
+	
+	do{
+		System("cls");
+	    printf("========Aplikasi Layanan Dokter Hewan========\n\n");
+    	puts("1. Registrasi\n");
+    	puts("2. Lihat Antrian\n");
+    	puts("3. Proses Pelayanan\n");
+    	puts("4. Keluar\n\n");
+    	printf("Menu Pilihan : \n"); scanf("%d", &menuPilihan);
+    	
+    	if(menuPilihan == 1){
+    		Registrasi(&Q);
+    		PrintQueue(Q);
+    		getch();
+		}
+		else if(menuPilihan == 2){
+			PrintQueue(Q);
+			puts("sfdsdf");
+		}
+		else if(menuPilihan == 3){
+			//belum
+		}
+		else if(menuPilihan == 4){
+			exit(0);
+		}
+		else{
+			printf("	  Masukan anda salah\n");
+			printf("	Masukan angka dari 1-4\n");
+			printf("	  Silakan coba lagi");
+		}
+
+	}
+	while(menuPilihan !=4);
+	return 0;
+}
+
