273268f2db60f85436e93c08efdd77bd14370309
Hilmabcd <78993982+Hilmabcd@users.noreply.github.com>
2021-08-01 05:57:49 -0600

    update lagi ampe cape

--- main.c
+++ main.c
@@ -19,47 +19,61 @@ int main (){
 	char menuPilihan;
 	
 	do{
-	printf("					====================================\n");
-	printf("					||      Layanan Dokter Hewan      ||\n");
-	printf("					||            NINUNINU            ||\n");
-	printf("					------------------------------------\n");
-	printf("					||     By: Hilma SR & Nisa HY     ||\n");
-	printf("					====================================\n");
-        printf("					||  1. Registrasi                 ||\n");
-        printf("					||  2. Lihat Antrian              ||\n");
-        printf("					||  3. Proses Pelayanan           ||\n");
-        printf("					||  4. Keluar                     ||\n");
-        printf("					====================================\n\n");
-        printf("					Menu Pilihan : "); scanf(" %c", &menuPilihan);
+	    system("cls");
+	    printf("					====================================\n");
+            printf("					||      Layanan Dokter Hewan      ||\n");
+	    printf("					||            NINUNINU            ||\n");
+	    printf("					------------------------------------\n");
+	    printf("					||     By: Hilma SR & Nisa HY     ||\n");
+	    printf("					====================================\n");
+	    printf("					||  1. Registrasi                 ||\n");
+	    printf("					||  2. Lihat Antrian              ||\n");
+	    printf("					||  3. Proses Pelayanan           ||\n");
+	    printf("					||  4. Keluar                     ||\n");
+	    printf("					====================================\n\n");
+	    printf("					Menu Pilihan : "); scanf(" %c", &menuPilihan);
     	
-    	if(menuPilihan == '1'){
+    	    if(menuPilihan == '1'){
+    		
     		Registrasi(&Q);
-    		system("pause");
+    		system("pause");	
+		
 		}
 		else if(menuPilihan == '2'){
+		
 			printQueue(Q);
 			system("pause");
+
 		}
 		else if(menuPilihan == '3'){
+		
 			prosesAntrian(&Q);
 			system("pause");
+
 		}
 		else if(menuPilihan == '4'){
+		
 			printf("					====================================\n");
 			printf("					||       Sampai Jumpa Lagi        ||\n");
 			printf("					||            NINUNINU            ||\n");
 			printf("					====================================\n");
 			exit(0);
+
 		}
 		else{
+		
 			printf("\n");
 			printf("					====================================\n");
 			printf("					||       Masukan Anda Salah       ||\n");
 			printf("					||       Masukan Angka 1-4!       ||\n");
 			printf("					||       Silakan Coba Lagi        ||\n");
 			printf("					====================================\n");
+			system("pause");
+	
 		}
+	
 	} while(menuPilihan != '4');
 	
 	return 0;
+
 }
