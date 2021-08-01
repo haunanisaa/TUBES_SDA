e485a847f5448239061cd1eaa9d738734ba34267
Hilmabcd <78993982+Hilmabcd@users.noreply.github.com>
2021-07-31 19:38:32 -0600

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
  * Deskripsi : main driver Aplikasi Layanan Dokter Hewan
  * Nama : Hilma Sri Rahayu
  * Tanggal : 22 Juli 2021
- * Modifikasi : 25 Juli 2021
+ * Update : 1 Agustus 2021
  */
 
 #include <stdio.h>
@@ -16,7 +16,7 @@ int main (){
 	Queue Q;
 	CreateQueue(&Q);
 	
-	int menuPilihan;
+	char menuPilihan;
 	
 	do{
 	printf("					====================================\n");
@@ -25,26 +25,26 @@ int main (){
 	printf("					------------------------------------\n");
 	printf("					||     By: Hilma SR & Nisa HY     ||\n");
 	printf("					====================================\n");
-    printf("					||  1. Registrasi                 ||\n");
-    printf("					||  2. Lihat Antrian              ||\n");
-    printf("					||  3. Proses Pelayanan           ||\n");
-    printf("					||  4. Keluar                     ||\n");
-    printf("					====================================\n\n");
-    printf("					Menu Pilihan : "); scanf("%d", &menuPilihan);
+        printf("					||  1. Registrasi                 ||\n");
+        printf("					||  2. Lihat Antrian              ||\n");
+        printf("					||  3. Proses Pelayanan           ||\n");
+        printf("					||  4. Keluar                     ||\n");
+        printf("					====================================\n\n");
+        printf("					Menu Pilihan : "); scanf("%c", &menuPilihan);
     	
-    	if(menuPilihan == 1){
+    	if(menuPilihan == '1'){
     		Registrasi(&Q);
     		system("pause");
 		}
-		else if(menuPilihan == 2){
+		else if(menuPilihan == '2'){
 			printQueue(Q);
 			system("pause");
 		}
-		else if(menuPilihan == 3){
+		else if(menuPilihan == '3'){
 			prosesAntrian(&Q);
 			system("pause");
 		}
-		else if(menuPilihan == 4){
+		else if(menuPilihan == '4'){
 			printf("					====================================\n");
 			printf("					||       Sampai Jumpa Lagi        ||\n");
 			printf("					||            NINUNINU            ||\n");
@@ -52,13 +52,14 @@ int main (){
 			exit(0);
 		}
 		else{
+			printf("\n");
 			printf("					====================================\n");
 			printf("					||       Masukan Anda Salah       ||\n");
 			printf("					||       Masukan Angka 1-4!       ||\n");
 			printf("					||       Silakan Coba Lagi        ||\n");
 			printf("					====================================\n");
 		}
-	} while(menuPilihan != 4);
+	} while(menuPilihan != '4');
 	
 	return 0;
 }
